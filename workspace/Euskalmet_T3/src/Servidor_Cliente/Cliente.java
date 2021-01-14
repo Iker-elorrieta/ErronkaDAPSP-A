ed45dba846ede6c60cd1c9b6597040be9c20a38c
MichelleSiranaula <michellestefania.siranaulasa@elorrieta-errekamari.com>
2021-01-14 13:22:39 +0100

    Cliente Servidor añadido

---
+++ workspace/Euskalmet_T3/src/Servidor_Cliente/Cliente.java
@@ -0,0 +1,54 @@
+package Servidor_Cliente;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class Cliente {
+
+	private final int PUERTO = 5000;
+	private final String IP = "192.168.106.28";
+	private String resultado ="";
+
+	public void iniciar() {
+		Socket cliente = null;
+		ObjectInputStream entrada = null;
+		ObjectOutputStream salida = null;
+		try {
+			cliente = new Socket(IP, PUERTO);
+			System.out.println("Conexion realizada con servidor");
+			entrada = new ObjectInputStream(cliente.getInputStream());
+			salida = new ObjectOutputStream (cliente.getOutputStream());
+			
+			salida.writeObject("SELECT nombre from municipio");
+			
+			resultado = entrada.readObject().toString();
+			
+			
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		} finally {
+			try {
+				if (cliente != null)
+					cliente.close();
+				if (entrada != null)
+					entrada.close();
+				if (salida != null)
+					salida.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println("Fin cliente");
+		}
+	}
+	public static void main(String[] args) {
+		Cliente c1 = new Cliente();
+		c1.iniciar();
+	}
+	public String getResultado() {
+		return resultado;
+	}
+}
