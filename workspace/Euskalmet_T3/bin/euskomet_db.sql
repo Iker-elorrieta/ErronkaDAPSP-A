570f6373e6ea4f75d6865e1730bffb8b1c6cbcc3
AitorUrru <aitor.urrutiaet@elorrieta-errekamari.com>
2021-01-22 10:20:51 +0100

    sql actualizado

--- workspace/Euskalmet_T3/bin/euskomet_db.sql
+++ workspace/Euskalmet_T3/bin/euskomet_db.sql
@@ -3,13 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-01-2021 a las 20:28:05
+-- Tiempo de generación: 22-01-2021 a las 10:17:40
 -- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.2.34
+-- Versión de PHP: 7.4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+01:00";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -20,8 +20,6 @@ SET time_zone = "+01:00";
 --
 -- Base de datos: `euskomet_db`
 --
-CREATE DATABASE IF NOT EXISTS `euskomet_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `euskomet_db`;
 
 -- --------------------------------------------------------
 
@@ -29,7 +27,6 @@ USE `euskomet_db`;
 -- Estructura de tabla para la tabla `calidad_aire`
 --
 
-DROP TABLE IF EXISTS `calidad_aire`;
 CREATE TABLE `calidad_aire` (
   `fecha_hora` datetime NOT NULL,
   `calidad` varchar(40) DEFAULT NULL,
@@ -42,7 +39,6 @@ CREATE TABLE `calidad_aire` (
 -- Estructura de tabla para la tabla `espacios_naturales`
 --
 
-DROP TABLE IF EXISTS `espacios_naturales`;
 CREATE TABLE `espacios_naturales` (
   `cod_enatural` int(20) NOT NULL,
   `nombre` varchar(40) DEFAULT NULL,
@@ -53,20 +49,12 @@ CREATE TABLE `espacios_naturales` (
   `foto` varchar(1000) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `espacios_naturales`
---
-
-INSERT INTO `espacios_naturales` (`cod_enatural`, `nombre`, `descripcion`, `tipo`, `latitud`, `longitud`, `foto`) VALUES
-(0, NULL, NULL, NULL, NULL, NULL, NULL);
-
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `estaciones`
 --
 
-DROP TABLE IF EXISTS `estaciones`;
 CREATE TABLE `estaciones` (
   `cod_estacion` int(20) NOT NULL,
   `nombre` varchar(40) DEFAULT NULL,
@@ -75,7 +63,6 @@ CREATE TABLE `estaciones` (
   `coord_y` double DEFAULT NULL,
   `latitud` double DEFAULT NULL,
   `longitud` double DEFAULT NULL,
-  `foto` varchar(1000) DEFAULT NULL,
   `cod_muni` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -85,7 +72,6 @@ CREATE TABLE `estaciones` (
 -- Estructura de tabla para la tabla `fav_espacios`
 --
 
-DROP TABLE IF EXISTS `fav_espacios`;
 CREATE TABLE `fav_espacios` (
   `cod_enatural` int(20) NOT NULL,
   `cod_usuario` int(20) NOT NULL
@@ -97,7 +83,6 @@ CREATE TABLE `fav_espacios` (
 -- Estructura de tabla para la tabla `fav_municipio`
 --
 
-DROP TABLE IF EXISTS `fav_municipio`;
 CREATE TABLE `fav_municipio` (
   `cod_muni` int(20) NOT NULL,
   `cod_usuario` int(20) NOT NULL
@@ -109,7 +94,6 @@ CREATE TABLE `fav_municipio` (
 -- Estructura de tabla para la tabla `hash`
 --
 
-DROP TABLE IF EXISTS `hash`;
 CREATE TABLE `hash` (
   `cod_hash` int(20) NOT NULL,
   `nombre` varchar(40) DEFAULT NULL,
@@ -123,12 +107,14 @@ CREATE TABLE `hash` (
 -- Estructura de tabla para la tabla `municipio`
 --
 
-DROP TABLE IF EXISTS `municipio`;
 CREATE TABLE `municipio` (
   `cod_muni` int(20) NOT NULL,
   `nombre` varchar(40) DEFAULT NULL,
   `descripcion` varchar(10000) DEFAULT NULL,
-  `cod_prov` int(20) NOT NULL
+  `cod_prov` int(20) NOT NULL,
+  `foto` varchar(1000) DEFAULT NULL,
+  `longitud` double DEFAULT NULL,
+  `latitud` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -137,10 +123,10 @@ CREATE TABLE `municipio` (
 -- Estructura de tabla para la tabla `muni_espacios`
 --
 
-DROP TABLE IF EXISTS `muni_espacios`;
 CREATE TABLE `muni_espacios` (
   `cod_muni` int(20) NOT NULL,
-  `cod_enatural` int(20) NOT NULL
+  `cod_enatural` int(20) NOT NULL,
+  `cod_muni_espacios` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -149,7 +135,6 @@ CREATE TABLE `muni_espacios` (
 -- Estructura de tabla para la tabla `provincia`
 --
 
-DROP TABLE IF EXISTS `provincia`;
 CREATE TABLE `provincia` (
   `cod_prov` int(20) NOT NULL,
   `nombre` varchar(40) DEFAULT NULL
@@ -161,37 +146,16 @@ CREATE TABLE `provincia` (
 -- Estructura de tabla para la tabla `usuario`
 --
 
-DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE `usuario` (
   `cod_usuario` int(11) NOT NULL,
   `nombre` varchar(40) DEFAULT NULL,
-  `contraseña` varchar(40) DEFAULT NULL,
-  `p_clave` varchar(40) DEFAULT NULL
+  `contraseña` varchar(150) DEFAULT NULL,
+  `p_clave` varchar(150) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `provincia`
---
-ALTER TABLE `provincia`
-  ADD PRIMARY KEY (`cod_prov`);
-
---
--- Indices de la tabla `municipio`
---
-ALTER TABLE `municipio`
-  ADD PRIMARY KEY (`cod_muni`),
-  ADD KEY `FK_PROV` (`cod_prov`);
-
---
--- Indices de la tabla `estaciones`
---
-ALTER TABLE `estaciones`
-  ADD PRIMARY KEY (`cod_estacion`),
-  ADD KEY `FK_MUNI_ESTACION` (`cod_muni`);
 
 --
 -- Indices de la tabla `calidad_aire`
@@ -207,24 +171,16 @@ ALTER TABLE `espacios_naturales`
   ADD PRIMARY KEY (`cod_enatural`);
 
 --
--- Indices de la tabla `muni_espacios`
+-- Indices de la tabla `estaciones`
 --
-ALTER TABLE `muni_espacios`
-  ADD PRIMARY KEY (`cod_muni`,`cod_enatural`),
-  ADD KEY `FK_MUNI_MUNIESPACIOS` (`cod_muni`),
-  ADD KEY `FK_ENATURALES_MUNIESPACIOS` (`cod_enatural`);
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`cod_usuario`);
+ALTER TABLE `estaciones`
+  ADD PRIMARY KEY (`cod_estacion`),
+  ADD KEY `FK_MUNI_ESTACION` (`cod_muni`);
 
 --
 -- Indices de la tabla `fav_espacios`
 --
 ALTER TABLE `fav_espacios`
-  ADD PRIMARY KEY (`cod_enatural`,`cod_usuario`),
   ADD KEY `FK_ENATURALES_FAV` (`cod_enatural`),
   ADD KEY `FK_USUARIO_FAVENATURALES` (`cod_usuario`);
 
@@ -232,7 +188,6 @@ ALTER TABLE `fav_espacios`
 -- Indices de la tabla `fav_municipio`
 --
 ALTER TABLE `fav_municipio`
-  ADD PRIMARY KEY (`cod_muni`,`cod_usuario`),
   ADD KEY `FK_MUNI_FAV` (`cod_muni`),
   ADD KEY `FK_USUARIO_FAVMUNI` (`cod_usuario`);
 
@@ -241,6 +196,33 @@ ALTER TABLE `fav_municipio`
 --
 ALTER TABLE `hash`
   ADD PRIMARY KEY (`cod_hash`);
+
+--
+-- Indices de la tabla `municipio`
+--
+ALTER TABLE `municipio`
+  ADD PRIMARY KEY (`cod_muni`),
+  ADD KEY `FK_PROV` (`cod_prov`);
+
+--
+-- Indices de la tabla `muni_espacios`
+--
+ALTER TABLE `muni_espacios`
+  ADD PRIMARY KEY (`cod_muni_espacios`),
+  ADD KEY `FK_MUNI_MUNIESPACIOS` (`cod_muni`),
+  ADD KEY `FK_ENATURALES_MUNIESPACIOS` (`cod_enatural`);
+
+--
+-- Indices de la tabla `provincia`
+--
+ALTER TABLE `provincia`
+  ADD PRIMARY KEY (`cod_prov`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`cod_usuario`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
