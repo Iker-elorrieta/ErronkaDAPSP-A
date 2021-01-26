aab2193f82981ec99530b448fb9a79cebc8c3e5c
in2dam <in2dam@HZ327378.elorrieta.int>
2021-01-26 13:42:01 +0100

    pruebas psp

--- workspace/Euskalmet_T3/src/PruebasPSP/Pruebas.java
+++ workspace/Euskalmet_T3/src/PruebasPSP/Pruebas.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
